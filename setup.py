665ed41000848587573b352e2fc74d02c0ae7771
Glutanimate <glutanimate@users.noreply.github.com>
2019-12-17 18:38:06 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="anki_testing",
+    version="0.1.0",
+    description="A small utility for testing Anki 2.1 addons",
+    author="Michal Krassowski, Aristotelis P. (Glutanimate)",
+    url="https://github.com/krassowski/anki_testing",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Software Development :: Testing",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="anki development testing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    python_requires=">=3.6"
+)
