0c2d86032a8ef7923dba81b3cd3331ae974395ac
Glutanimate <glutanimate@users.noreply.github.com>
2019-12-23 16:20:41 +0100

    Use cwd for tmp storage to avoid send2trash permission errors on linux

--- tests/test_anki_testing.py
+++ tests/test_anki_testing.py
@@ -1,5 +1,8 @@
 from anki_testing import anki_running
+import os
+
 
 def test_my_addon():
-    with anki_running() as anki_app:
+    with anki_running(base_path=os.getcwd(), keep_profile=True) as anki_app:
         assert True
+
