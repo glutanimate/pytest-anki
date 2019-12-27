df7ff28455b7074a097936de4bbc08d7add2bb71
Glutanimate <glutanimate@users.noreply.github.com>
2019-12-27 08:48:36 +0100

    Use /tmp for tests, lint

--- tests/test_anki_testing.py
+++ tests/test_anki_testing.py
@@ -1,16 +1,13 @@
-import os
 import pytest
 
 from anki_tests import anki_running, mw_addons_loaded, mw_profile_loaded
-
-ANKI_RUNNING_ARGS = dict(base_path=os.path.join(os.getcwd(), "anki_bases"))
 
 
 @pytest.mark.forked
 def test_anki_running():
     from aqt.qt import QApplication
 
-    with anki_running(**ANKI_RUNNING_ARGS) as anki_app:
+    with anki_running() as anki_app:
         assert isinstance(anki_app, QApplication)
 
 
@@ -18,7 +15,7 @@ def test_anki_running():
 def test_mw_addons_loaded():
     from anki.collection import _Collection
 
-    with anki_running(**ANKI_RUNNING_ARGS):
+    with anki_running():
         with mw_addons_loaded() as mw:
             assert mw.col is None
         assert isinstance(mw.col, _Collection)
@@ -28,7 +25,7 @@ def test_mw_addons_loaded():
 def test_mw_profile_loaded():
     from anki.collection import _Collection
 
-    with anki_running(**ANKI_RUNNING_ARGS):
+    with anki_running():
         with mw_profile_loaded() as mw:
             assert isinstance(mw.col, _Collection)
 
@@ -36,8 +33,8 @@ def test_mw_profile_loaded():
 @pytest.mark.forked
 def test_profile_loaded_hook():
     from anki.hooks import addHook
-    
-    with anki_running(**ANKI_RUNNING_ARGS):
+
+    with anki_running():
         foo = False
 
         with mw_addons_loaded():
@@ -47,16 +44,17 @@ def test_profile_loaded_hook():
                 foo = True
 
             addHook("profileLoaded", onProfileLoaded)
-            
+
             assert foo is False
-       
+
         assert foo is True
+
 
 @pytest.mark.forked
 def test_profile_unload_hook():
     from anki.hooks import addHook
-    
-    with anki_running(**ANKI_RUNNING_ARGS):
+
+    with anki_running():
         foo = False
 
         with mw_profile_loaded():
@@ -66,7 +64,7 @@ def test_profile_unload_hook():
                 foo = True
 
             addHook("unloadProfile", onProfileUnload)
-            
+
             assert foo is False
-        
+
         assert foo is True
