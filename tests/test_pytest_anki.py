32a980fc8d930b17ab55cacc71864780307a7580
Glutanimate <glutanimate@users.noreply.github.com>
2021-08-24 06:47:18 +0200

    Qt version test passed, so remove debug statement

--- tests/test_pytest_anki.py
+++ tests/test_pytest_anki.py
@@ -41,10 +41,6 @@ def test_anki_session_types(anki_session: AnkiSession):
     from aqt import AnkiApp
     from aqt.main import AnkiQt
     
-    from aqt import qtmajor, qtminor
-    
-    raise AssertionError(f"{qtmajor} {qtminor}")
-
     assert isinstance(anki_session.app, AnkiApp)
     assert isinstance(anki_session.mw, AnkiQt)
     assert isinstance(anki_session.user, str)
