f2afa770d8e8b599430483074fdf65f7fa78db8c
Glutanimate <5459332+glutanimate@users.noreply.github.com>
2025-04-09 22:42:28 +0200

    Disable Anki version compat warning for now

--- pytest_anki/plugin.py
+++ pytest_anki/plugin.py
@@ -42,20 +42,19 @@ from ._config import get_latest_tested_lib_versions
 from ._launch import anki_running
 from ._session import AnkiSession
 
+# def pytest_configure(config: "Config"):
+#     """Hook into pytest_configure stage to prepare plugin, e.g.
+#     in order to assert that runtime environment is supported"""
+#     latest_tested_lib_versions = get_latest_tested_lib_versions()
+#     anki_version = get_anki_version()
 
-def pytest_configure(config: "Config"):
-    """Hook into pytest_configure stage to prepare plugin, e.g.
-    in order to assert that runtime environment is supported"""
-    latest_tested_lib_versions = get_latest_tested_lib_versions()
-    anki_version = get_anki_version()
-
-    if anki_version > (latest_tested_anki_version := latest_tested_lib_versions.anki):
-        warning = Warning(
-            "The latest Anki version pytest-anki was tested with is"
-            f" {latest_tested_anki_version}. You are using {anki_version}. pytest-anki"
-            " might not behave as expected or not work at all."
-        )
-        config.issue_config_time_warning(warning, stacklevel=2)
+#     if anki_version > (latest_tested_anki_version := latest_tested_lib_versions.anki):
+#         warning = Warning(
+#             "The latest Anki version pytest-anki was tested with is"
+#             f" {latest_tested_anki_version}. You are using {anki_version}. pytest-anki"
+#             " might not behave as expected or not work at all."
+#         )
+#         config.issue_config_time_warning(warning, stacklevel=2)
 
 
 @pytest.fixture
